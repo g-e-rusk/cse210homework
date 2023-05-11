38e09ec204157307fcb67abc5a585ca7e3458e96
g-e-rusk <grusk92@gmail.com>
2023-05-11 12:40:36 -0700

    updated journal project

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -4,23 +4,39 @@
 public class Entry {
     public string _userResponse = "";
     public string _dateWritten = "";
-
     public string _promptUsed = "";
 
-    //public List<JournalIdeas> _dailyPrompt = new List<JournalIdeas>();
+    public Entry()
+    {
 
+    }
     
-    public void entryDisplay() {
+    // Method for getting the date as a string
+    public void GetDate() {
+        DateTime currentDate = DateTime.Now;
+        string _dateWritten = currentDate.ToShortDateString();
+        Console.WriteLine($"{_dateWritten}");
+    }
+
+    // Method for getting the randomly generated prompt
+    public void GetPrompt() {
         JournalIdeas entryPrompt = new JournalIdeas();
         entryPrompt.DisplayDailyPrompt();
         Console.Write("> ");
     }
 
-    public void addEntry() {
-        Console.Write($"{_dateWritten}");
-        Console.WriteLine($"{_promptUsed}");
+    // Method for getting user reponse
+    public void GetResponse() {
+        string _userResponse = Console.ReadLine();
+    }
+
+    // Method for how the entries will be displayed
+    public void DisplayEntry() {
+        Console.WriteLine($"{_dateWritten} - {_promptUsed}");
         Console.WriteLine($"{_userResponse}");
     }
+
+    
 }
 
         
--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,7 +1,24 @@
 // This is the template for my Journal class
 // This will add and entry, display all the entries, save to a file and load from a file
 // Calls the Entry display method
+using System;
+using System.Collections.Generic;
+using System.IO;
 
 public class Journal {
     
+    public List<Entry> _entries = new List<Entry>();
+
+
+    public Journal() 
+    {
+    }
+
+
+
+    
+
 }
+
+
+    
