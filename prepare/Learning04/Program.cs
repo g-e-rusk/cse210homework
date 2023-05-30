8a5e570e214525a3a07d21ecb3db077537c839ba
g-e-rusk <grusk92@gmail.com>
2023-05-30 15:56:27 -0700

    Finished Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Console.WriteLine();
+        Assignment a1 = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+        MathAssignment ma1 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(ma1.GetSummary());
+        Console.WriteLine(ma1.GetHomeworkList());
+        Console.WriteLine();
+        WritingAssignment wa1 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(wa1.GetSummary());
+        Console.WriteLine(wa1.GetWritingInformation());
+        Console.WriteLine();
     }
 }
